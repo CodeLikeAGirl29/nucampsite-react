415fb935f51249caab27ce33ea2b944319a4651e
Lindsey Howard <miss_leading009@yahoo.com>
2021-10-31 10:26:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
-import Main from './components/MainComponent';
 import './App.css';
+
+const store = ConfigureStore();
 
 class App extends Component {
     render() {
-        const store = ConfigureStore();
         return (
             <Provider store={store}>
-            <BrowserRouter>
-            <div className="App">
-                <Main />
-            </div>
-            </BrowserRouter>
+                <BrowserRouter>
+                    <div className="App">
+                        <Main />
+                    </div>
+                </BrowserRouter>
             </Provider>
         );
     };
